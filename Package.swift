00cfee0cdccb7c709cf52ed4352de8c6aa9f0c78
xcode73 <xcode.uln@gmail.com>
2024-02-20 15:38:41 +0400

    change to macos12

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "web-objects",
     platforms: [
-       .macOS(.v10_15)
+       .macOS(.v12)
     ],
     products: [
         .library(name: "WebObjects", targets: ["WebObjects"]),
