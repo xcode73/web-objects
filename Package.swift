58e3a656b37fb9e2201a8942a2639c5552f506bc
Nikolai Eremenko <nikolai.dev@icloud.com>
2024-06-08 14:36:41 +0400

    build: update dependencies

--- Package.swift
+++ Package.swift
@@ -10,8 +10,7 @@ let package = Package(
         .library(name: "WebObjects", targets: ["WebObjects"])
     ],
     dependencies: [
-        .package(path: "../feather-objects"),
-//        .package(url: "https://github.com/xcode73/feather-objects.git", branch: "test-dev")
+        .package(url: "https://github.com/xcode73/feather-objects", branch: "test-dev")
     ],
     targets: [
         .target(name: "WebObjects", dependencies: [
